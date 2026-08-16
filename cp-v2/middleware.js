c0146722f16018b06a3d4f3c6cff087756531702
armindogyf-cmyk <armindo.gyf@gmail.com>
2026-08-16 17:04:54 +0100

    CP V2.1: protect private routes

---
+++ cp-v2/middleware.js
@@ -0,0 +1,3 @@
+import { NextResponse } from 'next/server';
+export const config={matcher:['/((?!api/auth|login.html|_next|favicon.ico).*)']};
+export default async function middleware(req){const token=req.cookies.get('cp_session')?.value;if(!token){return NextResponse.redirect(new URL('/login.html',req.url));}return NextResponse.next();}
